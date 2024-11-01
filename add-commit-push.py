0ccb6e8f4b119d1faecd20768ea4542920e0a2a3
bkwan <brandonckwan@lewisu.edu>
2024-11-01 10:32:04 -0500

    Update files.

--- add-commit-push.py
+++ add-commit-push.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 def executeCommand(cmd):
     print(cmd)
@@ -8,6 +9,14 @@ def executeCommand(cmd):
 print("AddCommitPush")
 print("") 
 executeCommand("git status")
+
+response=input("Would you like to AddCommitPush (y/n)? ")
+print(response)
+
+# Check if the input is not equal to "y"
+if response != "y":
+    sys.exit()
+
 executeCommand("git add -A")
 executeCommand("git commit -m \"Update files.\"")
 executeCommand("git push")
